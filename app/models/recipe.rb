02b810fbde04364d0fadb1bb4b690afa673965da
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-02-05 20:50:03 +0000

    automatically backed up by learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,9 +2,4 @@ class Recipe < ActiveRecord::Base
   has_many :ingredients
   accepts_nested_attributes_for :ingredients
 
-  def ingredients_attributes=(ingredients)
-    self.ingredients = Ingredient.find_or_create_by(name: ingredients.name)
-    self.ingredients.update(ingredients)
-  end
-
 end
